03e7051dd2af7aee6d54045f2296f4c1c69a9a27
Quantamofsolace <rameshmanipatruni11@gmail.com>
2026-04-06 04:29:20 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,1 @@
-provider "aws" {
-  region = "ap-south-1"
-  
-}
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = ">5.0" #morthan 4.0 version
-    }
-  }
-}
